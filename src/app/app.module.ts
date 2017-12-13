87c1913666a7b87a340a9b64d0daceb11e0523bb
Edgar Hernández <edgarh2e>
2017-12-13 14:17:16 -0600

    First commit: angular 5 app generation.

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+    ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
